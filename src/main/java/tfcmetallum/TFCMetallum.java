02f19140acd7f4d5c344eaf57021aaebedeb1807
DisasterMoo <luan.santos.pinto@gmail.com>
2019-11-17 18:01:36 -0300

    Updated TFC dependency

--- src/main/java/tfcmetallum/TFCMetallum.java
+++ src/main/java/tfcmetallum/TFCMetallum.java
@@ -40,6 +40,5 @@ public class TFCMetallum
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-        VeinLoader.INSTANCE.postInit();
     }
 }
--- src/main/java/tfcmetallum/util/VeinLoader.java
+++ src/main/java/tfcmetallum/util/VeinLoader.java
@@ -2,37 +2,28 @@ package tfcmetallum.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Map;
 import java.util.Objects;
 
-import com.google.common.base.Strings;
-import com.google.gson.JsonParseException;
-import com.google.gson.reflect.TypeToken;
 import org.apache.commons.io.FileUtils;
 
 import net.dries007.tfc.world.classic.worldgen.vein.VeinRegistry;
-import net.dries007.tfc.world.classic.worldgen.vein.VeinType;
-import tfcmetallum.TFCMetallum;
 
-import static net.dries007.tfc.Constants.GSON;
-import static tfcmetallum.TFCMetallum.MODID;
+import static net.dries007.tfc.api.util.TFCConstants.MOD_ID;
 
 public enum VeinLoader
 {
     INSTANCE;
 
-    private static final String DEFAULT_ORE_SPAWN_LOCATION = "assets/tfcmetallum/config/ore_spawn_data.json";
-    private File worldGenFile;
+    private static final String DEFAULT_ORE_SPAWN_LOCATION = "assets/tfcmetallum/config/tfc_metallum_ores.json";
 
     public void preInit(File dir)
     {
-        File tfcDir = new File(dir, MODID);
+        File tfcDir = new File(dir, MOD_ID);
         if (!tfcDir.exists() && !tfcDir.mkdir())
         {
-            throw new Error("Problem creating TFC Metallum extra config directory.");
+            throw new Error("Problem creating TFC extra config directory.");
         }
-        worldGenFile = new File(tfcDir, "ore_spawn_data.json");
+        File worldGenFile = new File(tfcDir, "tfc_metallum_ores.json");
         try
         {
             if (worldGenFile.createNewFile())
@@ -43,33 +34,6 @@ public enum VeinLoader
         catch (IOException e)
         {
             throw new Error("Problem creating default ore vein config file.", e);
-        }
-    }
-
-    public void postInit()
-    {
-        String worldGenData;
-        try
-        {
-            worldGenData = FileUtils.readFileToString(worldGenFile, Charset.defaultCharset());
-        }
-        catch (IOException e)
-        {
-            throw new Error("Error reading ore vein config file.", e);
-        }
-
-        if (!Strings.isNullOrEmpty(worldGenData))
-        {
-            try
-            {
-                Map<String, VeinType> values = GSON.fromJson(worldGenData, new TypeToken<Map<String, VeinType>>() {}.getType());
-                values.forEach((name, abc) -> abc.getLooseRockItem());
-                values.forEach(VeinRegistry.INSTANCE::registerVein);
-            }
-            catch (JsonParseException e)
-            {
-                TFCMetallum.getLog().warn("Could not load TFC-Metallum ore generation config! TFC will not generate any TFC-Metallum ores!", e);
-            }
         }
     }
 }
